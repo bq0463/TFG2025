ff0deda40872af82ea87c86ae592402bf7ae16f1
alvaro <alvaro.hidalgo.lopez@alumnos.upm.es>
2025-05-22 14:15:48 +0200

    Haciendo memoria 2

--- config/mysqlConnection.js
+++ config/mysqlConnection.js
@@ -1,9 +1,10 @@
 import mysql from 'mysql2/promise';
 
+//myqsl workbench
 export const connection = await mysql.createConnection({
-    host: '127.0.0.1',  // Dirección IP del servidor MySQL
-    port: 3306,         // Puerto 
-    user: 'root',       // Usuario que usas en Workbench
+    host: '127.0.0.1',  
+    port: 3306,         
+    user: 'root',    
     password: '', 
     database: 'TFG2025' 
 });
