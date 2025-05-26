c6ba6175265a68dc399246d2aff1edd329b8cb98
alvaro <alvaro.hidalgo.lopez@alumnos.upm.es>
2025-05-26 19:18:26 +0200

    Actualizar horas de trabajo y eliminar lógica de cierre de sesión automática en PaginaInicio

--- view/src/Pages/PaginaInicio.jsx
+++ view/src/Pages/PaginaInicio.jsx
@@ -1,22 +1,7 @@
 import RegisterForm from "../Components/Forms/RegisterForm.jsx";
 import LoginForm from "../Components/Forms/LoginForm.jsx";
 import './inicio.css';
-import { useEffect } from "react";
 const PaginaInicio = () => {
-  useEffect(() => {
-    const logout = async () => {
-      try {
-        await fetch("http://localhost:5000/logout", {
-          method: "POST",
-          credentials: "include"
-        });
-      } catch (error) {
-        console.error("Error al cerrar sesión automáticamente", error);
-      }
-    };
-
-    logout();
-  }, []);
   return (
     <div className='PaginaInicio'>
       <header className='header'>
